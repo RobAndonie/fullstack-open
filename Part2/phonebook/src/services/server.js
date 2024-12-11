af64eb6b683de29b65046bd97370160a1642e570
RobAndonie <a01411863@tec.mx>
2024-12-11 09:59:40 +0100

    Changes to routes

--- Part2/phonebook/src/services/server.js
+++ Part2/phonebook/src/services/server.js
@@ -1,27 +1,21 @@
 import axios from "axios";
 
+const baseUrl = "/api/persons";
+
 const getNumbers = () => {
-  return axios
-    .get("http://localhost:3001/persons")
-    .then((response) => response.data);
+  return axios.get(baseUrl).then((response) => response.data);
 };
 
 const addNumber = (newPerson) => {
-  return axios
-    .post("http://localhost:3001/persons", newPerson)
-    .then((response) => response.data);
+  return axios.post(baseUrl, newPerson).then((response) => response.data);
 };
 
 const updateNumber = (id, newPerson) => {
-  return axios
-    .put(`http://localhost:3001/persons/${id}`, newPerson)
-    .then((response) => response.data);
+  return axios.put(`${baseUrl}/${id}`, newPerson).then((response) => response.data);
 };
 
 const deleteNumber = (id) => {
-  return axios
-    .delete(`http://localhost:3001/persons/${id}`)
-    .then((response) => response.data);
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
 };
 
 export { getNumbers, addNumber, updateNumber, deleteNumber };
