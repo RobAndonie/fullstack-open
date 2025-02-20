261eda6d89303f24c8ae583b5e5b3784336c4acd
RobAndonie <a01411863@tec.mx>
2025-02-20 08:13:12 -0600

    Part 4

--- Part4/blog/controllers/blogs.js
+++ Part4/blog/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
@@ -13,15 +15,20 @@ blogsRouter.post("/", async (request, response, next) => {
 
   try {
     const body = request.body;
+    const user = request.user;
 
     const newBlog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user.id,
     });
 
     const savedBlog = await newBlog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
     next(error);
@@ -50,8 +57,16 @@ blogsRouter.put("/:id", async (request, response, next) => {
 
 blogsRouter.delete("/:id", async (request, response, next) => {
   try {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
+    const blogToDelete = await Blog.findById(request.params.id);
+
+    if (!blogToDelete) {
+      return response.status(404).json({ error: "blog not found" });
+    }
+
+    await blogToDelete.deleteOne();
+    console.log(blogToDelete);
+
+    response.status(200).json(blogToDelete);
   } catch (error) {
     next(error);
   }
